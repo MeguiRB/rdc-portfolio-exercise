2ce728ca55484edaf8a62763e59ddac0d94271eb
MeguiRB <87879934+MeguiRB@users.noreply.github.com>
2021-12-18 17:29:40 +0000

    Made a function to change the theme

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
 
 // prints a message in the browser's dev tools console
 console.log("Hello 🌎");
+
+//Based on https://lukelowrey.com/css-variable-theme-switcher/
+document.documentElement.setAttribute("data-theme", "light");
+function changeTheme() {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  } else {
+    targetTheme = "light";
+  }
+  document.documentElement.setAttribute("data-theme", targetTheme);
+}
