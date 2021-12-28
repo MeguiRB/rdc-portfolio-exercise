13388d32ecde55e265ada5111741b99e4f1fd19b
MeguiRB <87879934+MeguiRB@users.noreply.github.com>
2021-12-28 14:01:28 +0000

    Added javascript to change icon of theme button

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ console.log("Hello 🌎");
 //Based on https://lukelowrey.com/css-variable-theme-switcher/
 document.documentElement.setAttribute("data-theme", "light");
 function changeTheme() {
+  var button = document.getElementById("themeButton");
   var currentTheme = document.documentElement.getAttribute("data-theme");
   if (currentTheme === "light") {
     targetTheme = "dark";
+    button.setAttribute("class", "far fa-sun fa-2x themeButton");
   } else {
     targetTheme = "light";
+    button.setAttribute("class", "far fa-moon fa-2x themeButton");
   }
   document.documentElement.setAttribute("data-theme", targetTheme);
 }
